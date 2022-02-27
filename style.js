ec0566c4d13443a5bb6d2713eab0a04c85a542c1
Ogeid03 <d1390dm@gmail.com>
2022-02-27 11:54:07 +0100

    fix

--- style.js
+++ style.js
@@ -147,12 +147,12 @@ window.onload = function(){
         //Check File API support
         if(window.File && window.FileList && window.FileReader)
         {
-            var filesInput = document.getElementsByClassName("files");
+            var filesInput = document.getElementById("files");
             
             filesInput.addEventListener("change", function(event){
                 
                 var files = event.target.files; //FileList object
-                var output = document.getElementsByClassName("result");
+                var output = document.getElementById("result");
                 
                 for(var i = 0; i< files.length; i++)
                 {
@@ -182,8 +182,7 @@ window.onload = function(){
                 }                               
                
             });
-        }
-        else
+        }  else
         {
             console.log("Your browser does not support File API");
         }
