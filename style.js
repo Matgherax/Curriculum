e1a535c89782010bd82ca83d802617e92115f2f9
Ogeid03 <d1390dm@gmail.com>
2022-02-27 12:22:04 +0100

    fix 3

--- style.js
+++ style.js
@@ -142,12 +142,12 @@ window.onload = function(){
         //Check File API support
         if(window.File && window.FileList && window.FileReader)
         {
-            var filesInput = document.getElementsById("files");
+            var filesInput = document.getElementById("files");
             
             filesInput.addEventListener("change", function(event){
                 
                 var files = event.target.files; //FileList object
-                var output = document.getElementsById("result");
+                var output = document.getElementById("result");
                 
                 for(var i = 0; i< files.length; i++)
                 {
